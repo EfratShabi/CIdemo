1fed9946baae8394f236cc29fb99b478d57bdffd
TehilaWinder <twinder1707@gmail.com>
2026-02-10 12:08:20 +0200

    add main and test_main

---
+++ src/main.py
@@ -0,0 +1,5 @@
+def print_name():
+    return "efrat shabi is a chmpion girl"
+print_name()
+
+
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from src.main import print_name
+import pytest
+
+
+def test_efrat_shabi():
+    assert print_name()=="efrat shabi is a chmpion girl"
+
